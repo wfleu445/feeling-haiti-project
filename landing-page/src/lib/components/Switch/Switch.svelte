cc9045b6e9be235dc455d1f0858a74a01b401236
Melchisedek Dulcio <mdulcio@visiostack.com>
2024-11-07 07:45:25 -0500

    feat: Create Switch component

---
+++ landing-page/src/lib/components/Switch/Switch.svelte
@@ -0,0 +1,22 @@
+<script lang="ts" generics="T, Value extends number | string">
+  import {clsx} from 'clsx';
+  type SwitchProps = {
+    options: T[]
+    getOptionValue: (option: T) => Value,
+    // todo generify this string to Snippet in the future
+    getOptionLabel: (option: T) => string
+    value: Value | undefined
+  }
+
+  let {options, getOptionValue, getOptionLabel, value = $bindable()}: SwitchProps = $props();
+</script>
+
+<div class="flex flex-row rounded bg-slate-100 w-fit overflow-hidden p-1">
+  {#each options as option, i (i)}
+    <button class={clsx('flex py-1 px-3 rounded', getOptionValue(option) === value && 'bg-blue-600 text-white')} onclick={() => {
+      value = getOptionValue(option);
+    }}>
+      {getOptionLabel(option)}
+    </button>
+  {/each}
+</div>
